11d41e6597d30fd5c38f430deb8145874c3b7ff8
avmsoa <andrevms@hotmail.com>
2022-08-03 09:21:41 -0300

    Doc - att car descriptions

--- src/libs/entity/carro.c
+++ src/libs/entity/carro.c
@@ -1,15 +1,19 @@
 #include <carro.h>
 
 Carro carroInit(int numCarPassageiros) {
-    Carro c = calloc (1, sizeof(*c));
-    c->numMaxPassageiros = numCarPassageiros;
-    return c;
+    if(numCarPassageiros < 0 ) {
+        return NULL;
+    }
+    
+    Carro car = calloc (1, sizeof(*car));
+    car->numMaxPassageiros = numCarPassageiros;
+    return car;
 }
 
-void carroPrint(Carro c){
-     if(c != NULL){
+void carroPrint(Carro car){
+     if(car != NULL){
         printf("Imprimindo carro\n");
-        printf("Tamanho do Carro : %d \n", c->numMaxPassageiros);
+        printf("Tamanho do Carro : %d \n", car->numMaxPassageiros);
     }else {
         printf("Carro mal definido\n");
     }
--- src/libs/entity/carro.h
+++ src/libs/entity/carro.h
@@ -4,20 +4,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 struct carro {
     int numMaxPassageiros;
 };
+
+
 
 /* Um Carro é um ponteiro para uma struct carro */
 typedef struct carro *Carro;
  
 //INIT's
-/* Cria um carro que pode receber um valor maximo de num_passageiros ao mesmo tempo*/
+
+/*
+ * \brief Inicializa um carro
+ * \param numCarPassageiros Integer Number of passengers that the car can take.
+ * \return A Pointer for an object Carro with numMaxPassageiros with the number received.
+ *         NULL if parameters less then 0.
+ * \info Object should be desaloc by user
+ */
 Carro carroInit(int numCarPassageiros);
 
 
 //PRINTS's
-/* Imprime as informações do carro*/
-void carroPrint(Carro c);
+/*
+ * \brief Print's car Informations
+ * \param car Pointer to a carro that will be printed
+ * 
+ * \info IF car NULL, will print an error message 
+ */
+void carroPrint(Carro car);
  
 #endif /* PASSAGEIRO_H_ */
